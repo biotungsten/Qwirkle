162f23d04a0d86ae94cb9f070f6f17920d6c9f2b
David Sauer <sauerdavid13@icloud.com>
2021-03-13 13:38:12 +0100

    added printing utils and fixed small bugs

--- src/GameState.py
+++ src/GameState.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2021 David Sauer
 
-import numpy as np
 from .utils.visualisation import stone
+import copy
 
 SYMBOLS = ["✷", "◆", "✣", "✚", "●", "■"]
 COLORS = ["R", "G", "O", "Y", "P", "B"]
@@ -11,7 +11,7 @@ class GameState:
         self.playerA = Player(self)
         self.playerB = Player(self)
         self.playground = Playground(self)
-        self.freeStones = StoneCollection(self)
+        self.freeStones = StoneCollection(self, self)
         self.events = [] #controlled by controll class
         self.round = 0
 
@@ -19,16 +19,20 @@ class Playground:
     def __init__(self, game):
         self.game = game
         self.stones = StoneCollection(self, game, empty=True)
+    def __str__(self):
+        return str(self.stones)
 
 class Player:
     def __init__(self, game):
         self.game = game
         self.stones = StoneCollection(self, game, empty=True)
+    def __str__(self):
+        return str(self.stones)
 
 class StoneCollection:
     def __init__(self, parent, game, empty=False):
         self.parent = parent
-        self.stones = np.array()
+        self.stones = []
         self.m = 0
         self.game = game
         if empty:
@@ -37,7 +41,7 @@ class StoneCollection:
             for symbol in SYMBOLS:
                 for color in COLORS:
                     for n in range(3):
-                        self.stones.append(Stone(parent, symbol, color, n, self.m, self.game))
+                        self.stones.append(Stone(self, symbol, color, n, self.m, self.game))
                         self.m += 1
     def remove(self, idx ):
         self.stones[idx].valid = False
@@ -45,6 +49,16 @@ class StoneCollection:
         self.stones.append(item)
         item.idx = self.m
         self.m += 1
+    def __str__(self):
+        output = ""
+        n = -1
+        for stone in list(self.stones):
+            if stone.valid:
+                n+=1
+                if n%10 == 0 and n!=0:
+                    output += "\n"
+                output += str(stone) + "\t"
+        return output
 
 class Stone:
     def __init__(self, parent, symbol, color, id, idx, game):
@@ -56,9 +70,10 @@ class Stone:
         self.valid = True
         self.game = game
     def __str__(self):
-        return stone([self.symbol, self.color])
+        return stone([self.symbol, self.color]) + "-{}".format(self.id)
     def move(self, to):
-        self.parent.remove(self.idx)
-        to.append(self)
+        if self.valid:
+            to.append(copy.deepcopy(self))
+            self.parent.remove(self.idx)
 
 #TODO: Facilities for moving stones and printing stones, condition counting
